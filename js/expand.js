998be926abefceaedfd28ccb9c416de280eff9b3
Kevin Andries <andrieskevin5320@gmail.com>
2014-02-20 09:51:33 +0100

    update

--- js/expand.js
+++ js/expand.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$(".arrow").click(function(){
 		$(this).toggleClass("rotate");
-		$(this).parent().children(".toggle-text").slideToggle("300");
+		$(this).siblings(".toggle-text").slideToggle("300");
 	});
 });
