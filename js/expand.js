5751fec3d7b15d9b65b4c220a6a41a1945727e25
Kevin Andries <andrieskevin5320@gmail.com>
2014-02-25 13:00:53 +0100

    nav update

--- js/expand.js
+++ js/expand.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
 		$(this).siblings(".toggle-text").slideToggle("300");
 	});
 
+	$(".komm-arrow").click(function(){
+		$(this).parents("p").siblings(".inline-komm").slideToggle("300");
+	});
 	$(".se-komm").click(function(){
 		$(this).parents("p").siblings(".inline-komm").slideToggle("300");
 	});
